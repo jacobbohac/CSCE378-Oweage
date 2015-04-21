36ce5356393be0bd14df4c204f9a7d51d76db26d
jbohac <jacob.bohac@gmail.com>
2015-04-21 13:13:24 -0500

    Modified event page, added resources for icons

--- app/src/main/java/com/example/jacob/oweage/EventPage.java
+++ app/src/main/java/com/example/jacob/oweage/EventPage.java
@@ -1,18 +1,40 @@
 package com.example.jacob.oweage;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 
 public class EventPage extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+      //  this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, CONTACTS);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] CONTACTS = new String[] {
+            "PRIBNOW",
+            "NEUKIRCH",
+            "LEE",
+            "BOHAC",
+            "BRIDGES",
+            "ZANY",
+            "PLOOPERD AOFP"
+    };
 
 
     @Override
@@ -22,6 +44,15 @@ public class EventPage extends ActionBarActivity {
         return true;
     }
 
+    public void goHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goContacts(View view){
+        Intent intent = new Intent(this, ContactsPage.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
