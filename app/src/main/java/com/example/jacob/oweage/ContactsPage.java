85540dd1a71ff4cf113ac813dc584ecdbf48e72f
Pribs2 <pribs2.6@gmail.com>
2015-04-20 22:39:00 -0500

    Added main page and contacts page.

--- app/src/main/java/com/example/jacob/oweage/ContactsPage.java
+++ app/src/main/java/com/example/jacob/oweage/ContactsPage.java
@@ -1,45 +1,26 @@
 package com.example.jacob.oweage;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ContactsPage extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contacts_page);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_contacts_page, menu);
         return true;
     }
-
-    //Button btn = (Button)findViewById(R.id.button3);
-
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, ContactsPage.class);
-        startActivity(intent);
-    }
-
-    /*
-    btn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, ContactsPage.class));
-        }
-    });  */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
