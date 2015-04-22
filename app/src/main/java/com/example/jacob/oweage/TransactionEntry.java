cad678231ead64f7b393ddc7de206fb6050b1ef3
bnewkirk <bnewkirk94@hotmail.com>
2015-04-21 20:12:19 -0500

    created Contact and TransactionEntry classes

--- app/src/main/java/com/example/jacob/oweage/TransactionEntry.java
+++ app/src/main/java/com/example/jacob/oweage/TransactionEntry.java
@@ -14,4 +14,27 @@ public class TransactionEntry {
         this.amount = amount;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
